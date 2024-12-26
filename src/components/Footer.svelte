027a66458618624d65a2aa11913451a2ddb2fe7a
victor.code <vdcloayza@gmail.com>
2024-12-26 14:35:45 -0400

    ✅ ready for use

---
+++ src/components/Footer.svelte
@@ -0,0 +1,23 @@
+<footer>
+
+    <p>
+        creado por: <a href="https://github.com/moonhuntercode" target="_blank" rel="noreferrer">&copy; &reg; victor code</a>
+        
+        <!-- marca registrada html symbol, la que tiene R -->
+
+        
+         
+
+    </p>
+</footer>
+<style>
+footer{
+    color:white;
+    grid-area: footer;
+    width: var(--one-hundred-percent);
+    height: var(--one-hundred-percent);
+    background: var(--main-orange);
+    display: grid;
+    place-content: center;
+}    
+</style>
---
+++ src/components/Navbar.svelte
@@ -0,0 +1,18 @@
+<nav>
+  <a href="/">Home</a>
+  <a href="/">About</a>
+  <a href="/">Contact</a>
+</nav>
+
+<style>
+  nav {
+    grid-area: nav;
+    position: fixed;
+    width: var(--one-hundred-percent);
+    height: 13vh;
+    background: var(--main-orange);
+    display: flex;
+    flex-flow: row wrap;
+    place-content: center space-around;
+  }
+</style>
